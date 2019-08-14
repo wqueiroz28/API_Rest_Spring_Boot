450e7bf22b733315df1f08995ce135b530822828
wqueiroz28 <willqueiroz77@hotmail.com>
2019-08-14 17:50:02 -0300

    Projeto ApiRest

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,49 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.Topicoform;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@GetMapping
+	public List<TopicoDto> lista(String nomeCurso) {
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+	}
+	
+	@PostMapping
+	public void  cadastrar(@RequestBody Topicoform topicoform) {
+		
+		Topico topico = topicoform.converter();
+		topicoRepository.save(topico);
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/br/com/alura/forum/controller/form/Topicoform.java
@@ -0,0 +1,42 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+public class Topicoform {
+	
+	private String titulo;
+	private String mensagem;
+	private String nomeCurso;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	public Topico converter(CursoRepository repository) {
+		
+		Curso curso = CursoRepository.findBynome();
+		// TODO Auto-generated method stub
+		return new Topico(titulo,mensagem,curso);
+	}
+	
+	
+	
+
+}
